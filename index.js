c109f77cb161c7b02d441e4a0a5e02a4d5a8f916
hitesh1001 <107828120+hitesh1001@users.noreply.github.com>
2024-04-02 15:09:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function showSidebar(){
+    const forMobile = document.querySelector(".forMobile");
+    forMobile.style.display = "flex";
+}
+
+
+function Hidebar(){
+    const forMobile = document.querySelector(".forMobile");
+    forMobile.style.display = "none";
+
+}
+
